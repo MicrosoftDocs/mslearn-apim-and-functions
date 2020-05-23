303f0b38cef8d690dd5cca98f4c9b3e0cdc129e4
Robert McMurray <rmcmurray@users.noreply.github.com>
2020-05-23 06:34:12 -0700

    Update setup.sh
    The existing *setup.sh* script no longer worked as expected. (See AB#172643 and other bugs.) After publishing the functions, the Azure portal would not list the functions. This was because the existing Zip files contained more code than was necessary for the newer implementation of Azure functions. With that in mind, this new script extracts the contents of the Zip files and builds a deployment-ready Zip package with just the necessary files, while leaving the original Zip files with all of the original code in this repo.

--- setup.sh
+++ setup.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
+
 # Set up randomized names for a storage account and two functions, which must be globally unique
-STORAGE_ACCOUNT_NAME=storestorage$(openssl rand -hex 5)
-PRODUCT_FUNCTION_NAME=ProductFunction$(openssl rand -hex 5)
-ORDER_FUNCTION_NAME=OrderFunction$(openssl rand -hex 5)
-# Get the resource group
-RESOURCE_GROUP=$(az group list --query "[0].name" -o tsv)
-printf "The resource group is called $RESOURCE_GROUP\n"
+export STORAGE_ACCOUNT_NAME=storestorage$(openssl rand -hex 5)
+export PRODUCT_FUNCTION_NAME=ProductFunction$(openssl rand -hex 5)
+export ORDER_FUNCTION_NAME=OrderFunction$(openssl rand -hex 5)
+
+# Get the resource group and location
+export RESOURCE_GROUP=$(az group list --query "[0].name" -o tsv)
+export LOCATION=$(az group show --name $RESOURCE_GROUP | jq -r ".location")
+printf "\nThe resource group is called $RESOURCE_GROUP and is located in $LOCATION\n"
+
 # Create a storage account
-printf "Creating a storage account for the functions...\n"
-az storage account create --name $STORAGE_ACCOUNT_NAME --location westus --resource-group $RESOURCE_GROUP  --sku Standard_LRS
-printf "Storage account created.\n"
+printf "\nCreating a storage account for the functions...\n"
+az storage account create --name $STORAGE_ACCOUNT_NAME --location $LOCATION --resource-group $RESOURCE_GROUP  --sku Standard_LRS
+printf "\nStorage account created.\n"
+
 # Create two functions in Azure
-printf "Creating function apps in Azure...\n"
-az functionapp create --resource-group $RESOURCE_GROUP --consumption-plan-location centralus --name $PRODUCT_FUNCTION_NAME --storage-account $STORAGE_ACCOUNT_NAME --runtime dotnet
-az functionapp create --resource-group $RESOURCE_GROUP --consumption-plan-location centralus --name $ORDER_FUNCTION_NAME --storage-account $STORAGE_ACCOUNT_NAME --runtime dotnet
-printf "Function apps created.\n"
+printf "\nCreating function apps in Azure...\n"
+az functionapp create --resource-group $RESOURCE_GROUP --consumption-plan-location $LOCATION --name $PRODUCT_FUNCTION_NAME --storage-account $STORAGE_ACCOUNT_NAME --runtime dotnet --functions-version 2
+az functionapp create --resource-group $RESOURCE_GROUP --consumption-plan-location $LOCATION --name $ORDER_FUNCTION_NAME --storage-account $STORAGE_ACCOUNT_NAME --runtime dotnet --functions-version 2
+printf "\nFunction apps created.\n"
+
+# Building the zip files for the functions
+printf "\nBuilding zip files for deployment...\n"
+for zipFile in ProductDetailsFunc OrderShippingFunc
+do
+    mkdir $zipFile
+    unzip -q $zipFile.zip -d $zipFile
+    cd $zipFile/bin/Debug/netcoreapp2.1
+    zip -q -r ~/OnlineStoreFuncs/$zipFile.PKG.zip ./*
+    cd ~/OnlineStoreFuncs/
+done
+printf "\nZip files built.\n"
+
 # Use zip deploy to publish the code
-printf "Publishing function source code...\n"
-az functionapp deployment source config-zip -g $RESOURCE_GROUP -n $PRODUCT_FUNCTION_NAME --src ProductDetailsFunc.zip
-az functionapp deployment source config-zip -g $RESOURCE_GROUP -n $ORDER_FUNCTION_NAME --src OrderShippingFunc.zip
-printf "Code published. Functions are ready to use.\n"
+printf "\nPublishing function source code...\n"
+az functionapp deployment source config-zip -g $RESOURCE_GROUP -n $PRODUCT_FUNCTION_NAME --src ProductDetailsFunc.PKG.zip
+az functionapp deployment source config-zip -g $RESOURCE_GROUP -n $ORDER_FUNCTION_NAME --src OrderShippingFunc.PKG.zip
+printf "\nCode published. Functions are ready to use.\n"
